cb742ce0cccd19abd6f30fe63f011bd18bbeb465
Amin Ziagham <amin.ziagham@outlook.com>
2026-04-04 21:26:52 +0200

    Add RuleCondition EF Core entity configuration
    Introduced RuleConditionConfiguration to define database mapping for the RuleCondition entity. This includes primary key setup, required fields, string length constraints, and specifying the "jsonb" column type for the ValueJson property.
    
    #45

---
+++ src/Vectra.Infrastructure.Persistence.Sqlite/EntityConfigurations/RuleConditionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vectra.Core.Entities;
+
+namespace Vectra.Infrastructure.Persistence.Sqlite.EntityConfigurations;
+
+public class RuleConditionConfiguration : IEntityTypeConfiguration<RuleCondition>
+{
+    public void Configure(EntityTypeBuilder<RuleCondition> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).IsRequired();
+        builder.Property(e => e.RuleId).IsRequired();
+        builder.Property(e => e.LogicalOperator).HasMaxLength(4);
+        builder.Property(e => e.Attribute).HasMaxLength(4096);
+        builder.Property(e => e.Operator).HasMaxLength(16);
+
+        builder.Property(e => e.ValueJson).HasColumnType("jsonb");
+    }
+}
