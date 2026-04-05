790aab2b14e2a1c357dad78d121b346273ceb901
Amin Ziagham <amin.ziagham@outlook.com>
2026-04-05 22:01:28 +0200

    Add DI setup, dispatcher extensions, and project file
    Added AddVectraApplication for DI registration of handlers and validators. Introduced DispatcherExtensions for simplified action dispatching. Created Vectra.Application.csproj targeting .NET 10.0 with necessary dependencies and resource configuration.
    
    #72

---
+++ src/Vectra.Application/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Vectra.Application.Abstractions.Dispatchers;
+
+namespace Vectra.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddVectraApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Get all handler types
+        var handlerTypes = assembly.GetTypes()
+            .Where(t => t.GetInterfaces().Any(i =>
+                i.IsGenericType &&
+                i.GetGenericTypeDefinition() == typeof(IActionHandler<,>)))
+            .ToList();
+
+        // Register each handler
+        foreach (var handlerType in handlerTypes)
+        {
+            var interfaceType = handlerType.GetInterfaces()
+                .First(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IActionHandler<,>));
+
+            services.AddScoped(interfaceType, handlerType);
+        }
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
---
+++ src/Vectra.Application/Extensions/DispatcherExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Vectra.Application.Abstractions.Dispatchers;
+using Vectra.Application.Features.Agents.RegisterAgent;
+using Vectra.Application.Features.Authentications.GenerateToken;
+using Vectra.BuildingBlocks.Results;
+
+namespace Vectra.Application.Extensions;
+
+public static class DispatcherExtensions
+{
+    #region Agents
+    public static Task<Result<CreateAgentResult>> RegisterAgent(
+        this IDispatcher dispatcher,
+        CreateAgentRequest request,
+        CancellationToken cancellationToken)
+    {
+        return dispatcher.Dispatch(request, cancellationToken);
+    }
+    #endregion
+
+    #region Authentication
+    public static Task<Result<GenerateTokenResult>> GenerateToken(
+        this IDispatcher dispatcher,
+        GenerateTokenRequest request,
+        CancellationToken cancellationToken)
+    {
+        return dispatcher.Dispatch(request, cancellationToken);
+    }
+    #endregion
+}
